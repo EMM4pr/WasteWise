10536f02cd39f13e41ec806df5bf52e705f80325
alexandraholz <alexandraholz.b@gmail.com>
2023-11-27 16:56:21 +0100

    model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,3 @@
 class Category < ApplicationRecord
+  has_many :waste_items
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,4 @@
 class Location < ApplicationRecord
+  has_many :disposal_records
+  has_many :location_bin_types
 end
