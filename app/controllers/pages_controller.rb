ecc4b25a2139d7b08ffa1363bf6c7aeefe643b2a
emmapreysing <emma@preysingmail.com>
2023-11-29 13:17:46 +0100

    Fixed Navbar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -14,8 +14,10 @@ class PagesController < ApplicationController
     @waste_items = current_user.waste_items
     @users = current_user.waste_items
 
-    @disposal_record = current_user.disposal_records
+    @disposal_records = current_user.disposal_records
     @bin_types_count = current_user.disposal_records.joins(waste_item: :bin_type).group("bin_types.name").count
+    # @bin_type_name = current_user.disposable_records.
+    # @bin_type_description = current_user.disposable_records
     # Bin Type A => x mal in disposal_record of the user
     # {"Blue Paper Bin"=>3, "Yellow Bag"=>3, "Yellow Bin"=>4}
     # @all_values = bin_type_count.values
