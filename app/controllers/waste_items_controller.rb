9cca34bae4a5ceebb7ed035d74f569ef1bdad7a0
emmapreysing <emma@preysingmail.com>
2023-11-29 11:31:52 +0100

    Create frontend files

--- app/controllers/waste_items_controller.rb
+++ app/controllers/waste_items_controller.rb
@@ -24,14 +24,11 @@ class WasteItemsController < ApplicationController
   end
 
   def create
-
     @waste_item = Waste_items.new(waste_item_params)
-
-
     # save user of wasteitem appliance as current user
-    if user_signed_in?
-      @waste_item.user = current_user
-    end
+
+    @waste_item.user = current_user if user_signed_in?
+
     if @waste_item.save! # ! stop execution @ prob
       redirect_to waste_item_path(@waste_item), notice: 'your waste_items appliance was successfully created.'
     else
