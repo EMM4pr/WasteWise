319a320d5e348d38d2a8d9150cd8c155cb9eacf8
Shubham Gautam <gautam.shubham@gmail.com>
2023-11-30 14:17:21 +0100

    Fixed Google

--- app/controllers/visions_controller.rb
+++ app/controllers/visions_controller.rb
@@ -1,1 +1,13 @@
 
+class VisionsController < ApplicationController
+  def analyze
+    # @result = KindsOfBinService.determine_bin(params[:image_url])
+    @result = GoogleApiService.analyze_image(params[:image_url])
+
+    redirect_to visonresult_path(result: @result)
+  end
+
+  def vision_analyze
+    @trash = params[:result]
+  end
+end
