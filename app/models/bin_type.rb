62deabbef57889c82c4b01a5a162e60adfdfb4cb
Shubham Gautam <gautam.shubham@gmail.com>
2023-12-05 17:57:08 +0100

    Fixed Seeds

--- app/models/bin_type.rb
+++ app/models/bin_type.rb
@@ -1,4 +1,5 @@
 class BinType < ApplicationRecord
   has_many :waste_items, dependent: :destroy
   has_many :location_bin_types
+  has_one_attached :photo
 end
--- app/models/waste_item.rb
+++ app/models/waste_item.rb
@@ -1,12 +1,11 @@
 class WasteItem < ApplicationRecord
   belongs_to :user
   belongs_to :bin_type
-  belongs_to :category
   has_one :disposal_record
   has_one_attached :photo
 
   after_commit :find_waste_item_name, on: :create
-
+  
   def find_waste_item_name
     FindWasteNamesJob.perform_now(self.id)
   end
