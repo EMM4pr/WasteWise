fd3299dce271d8cd06ebeb680c0762bb130adb43
emmapreysing <emma@preysingmail.com>
2023-12-06 10:24:48 +0100

    No reward changes -> fixed errors & Image styling

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-
-  def level
-    
-  end
 end
